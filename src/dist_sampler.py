13cf37053905706e3d2e8dd107ccc693a9a260d1
pjdrm <pjdrm@ist.utl.pt>
2016-07-07 12:57:14 -0400

    Changed theta sample. Optimized L sample with matrix operation only.
    TODO: solve underflow problems

--- src/dist_sampler.py
+++ src/dist_sampler.py
@@ -3,13 +3,13 @@ Created on Jun 17, 2016
 
 @author: pedrom
 '''
-import numpy
+import numpy as np
 
 def sample_beta(pi0, pi1):
-    return numpy.random.beta(pi0, pi1)
+    return np.random.beta(pi0, pi1)
 
 def sample_bernouli(pi, n_samples):
-    return numpy.random.binomial(1, pi, size=n_samples)
+    return np.random.binomial(1, pi, size=n_samples)
 
 def sample_dirichlet(alpha):
-    return numpy.random.dirichlet(alpha)
+    return np.random.dirichlet(alpha)
--- src/model.py
+++ src/model.py
@@ -14,6 +14,8 @@ class ModelState(object):
         self.L = np.zeros(N)
         self.C0 = 0
         self.C1 = 0 
+        self.word_counts_L0 = self.label_word_counts(corpus, 0)
+        self.word_counts_L1 = self.label_word_counts(corpus, 1)
         self.theta0 = None
         self.theta1 = None
         self.gamma_pi0 = gamma_pi0
@@ -26,7 +28,17 @@ class ModelState(object):
         self.theta1 = sample_dirichlet(self.gamma_theta)
         self.C1 = np.count_nonzero(self.L)
         self.C0 = self.N - self.C1
+    
+    def label_word_counts(self, corpus, label):
+        if label == 0:
+            docs = corpus.docs_L0
+        else:
+            docs = corpus.docs_L0
         
+        word_counts = corpus.W_D_matrix[docs].sum(0)
+        return word_counts
+        
+            
     def sample_L(self):
         print("current L\n %s" % (str(self.L)))
         post_L = [self.sample_Lj(j) for j in range(len(self.L))]
@@ -36,8 +48,11 @@ class ModelState(object):
     def sample_Lj(self, j):
         if self.L[j] == 0:
             self.C0 = self.C0 - 1.0
+            self.word_counts_L0 -= self.corpus.W_D_matrix[j]
         else:
             self.C1 = self.C1 - 1.0
+            self.word_counts_L1 -= self.corpus.W_D_matrix[j]
+            
         print("C0 %f C1 %f"  % (self.C0, self.C1))
         #Computing for x = 0 (Lj = 0)
         factor1_log_L0 = np.log((self.C0 + self.gamma_pi0 - 1.0) / (self.C0 + self.C1 + self.gamma_pi0 + self.gamma_pi1 - 1.0))
@@ -45,26 +60,36 @@ class ModelState(object):
         #Computing for x = 1 (Lj = 1)
         factor1_log_L1 = np.log((self.C1 + self.gamma_pi1 - 1.0) / (self.C1 + self.C0 + self.gamma_pi0 + self.gamma_pi1 - 1.0))
         
+        '''
         factor2_log_L0 = 0.0
         factor2_log_L1 = 0.0
         for i in range(self.corpus.V):  
             #print(factor2_log_L0)
-            factor2_log_L0 += np.log(self.theta0[i]**self.corpus.W_D_matrix[j, i])
-            factor2_log_L1 += np.log(self.theta1[i]**self.corpus.W_D_matrix[j, i])
-            
+            factor2_log_L0 += np.log(self.theta0[i])*self.corpus.W_D_matrix[j, i]
+            factor2_log_L1 += np.log(self.theta1[i])*self.corpus.W_D_matrix[j, i]
+        '''
+        factor2_log_L0 = (np.log(self.theta0)*self.corpus.W_D_matrix[j].toarray()).sum(1)
+        factor2_log_L1 = (np.log(self.theta1)*self.corpus.W_D_matrix[j].toarray()).sum(1)
+        
         log_val0 = factor1_log_L0 + factor2_log_L0
         log_val1 = factor1_log_L1 + factor2_log_L1
         
         post_L0 = np.exp(log_val0 - np.logaddexp(log_val0, log_val1))
         post_L1 = np.exp(log_val1 - np.logaddexp(log_val0, log_val1))
+        print("factor1_log_L0: %s factor2_log_L0 %s post_L0: %s" % (str(factor1_log_L0), str(factor2_log_L0), str(post_L0)[1:-1]))
+        print("factor1_log_L1: %s factor2_log_L1 %s post_L1: %s" % (str(factor1_log_L1), str(factor2_log_L1), str(post_L1)[1:-1]))
+        if post_L0 == 0.0 or post_L1 == 0.0:
+            print("")
         coin_weight = sample_beta(post_L0, post_L1)
-        print("post_L0: %f post_L1: %f post_pi: %f" % (post_L0, post_L1, coin_weight))
+        print("post_pi: %f" % (coin_weight))
         Lj_new = sample_bernouli(coin_weight, 1)
         self.L[j] = Lj_new
         if Lj_new == 0:
             self.C0 += 1
+            self.word_counts_L0 += self.corpus.W_D_matrix[j]
         else:
             self.C1 += 1
+            self.word_counts_L1 += self.corpus.W_D_matrix[j]
             
     def sample_Theta0(self):
         post_theta0 = self.sample_Theta(self.gamma_theta, 0)
@@ -78,15 +103,11 @@ class ModelState(object):
         
     def sample_Theta(self, gamma_theta_prior, Cx):
         if Cx == 0:
-            docs = self.corpus.docs_L0
+            word_counts = self.word_counts_L0
         else:
-            docs = self.corpus.docs_L1
+            word_counts = self.word_counts_L1
         
-        t = np.zeros(self.corpus.V)
-        for i in range(len(t)):
-            NCxi = self.count_wi(docs, i)
-            gamma_theta_i = gamma_theta_prior[i]
-            t[i] = NCxi + gamma_theta_i
+        t = word_counts + gamma_theta_prior
         return sample_dirichlet(t)
             
     def count_wi(self, docs, wi):
